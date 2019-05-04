84569c536ea1f0545bbc9684891801c5c7a781f3
Thinmy Patrick Alves <thinmy@gmail.com>
2019-05-04 00:43:45 -0300

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='celery-strategy',
-    version='0.0.1',
+    version='0.0.2',
     description='Don`t lose any task when celery is offline or not responding.',
     author='Thinmy Patrick Alves',
     author_email='thinmy@gmail.com',
